04fc9e307a95c4630e98c4ff236f0fea360fce06
hardik jain <hardikj2003@gmail.com>
2026-03-04 08:27:37 +0530

    merge sort revised

--- Codes/revision.cpp
+++ Codes/revision.cpp
@@ -1,69 +1,76 @@
 #include <iostream>
 using namespace std;
 
-void display(int* arr, int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
+void merge(int* arr, int s, int e){
+    int mid = s + (e-s)/2;
 
-void bubbleSort(int* arr, int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0; j<n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+    int len1 = mid-s+1;
+    int len2 = e-mid;
+
+    int* first = new int[len1];
+    int* second = new int[len2];
+
+    int k = s;
+    for(int i=0; i<len1; i++){
+        first[i] = arr[k++];
+    }
+    k=mid+1;
+    for(int i=0;i<len2; i++){
+        second[i] = arr[k++];
+    }
+
+    int index1 =0;
+    int index2=0;
+
+    k = s;
+
+    while(index1<len1 && index2<len2){
+        if(first[index1] < second[index2]){
+            arr[k++] = first[index1++];
+        }else{
+            arr[k++] = second[index2++];
         }
+    }   
+
+    while(index1<len1){
+        arr[k++] = first[index1++];
     }
+    while(index2<len2){
+        arr[k++] = second[index2++];
+    }
+
+    delete []first;
+    delete []second;
+
 }
 
-void selectionSort(int* arr, int n){
-    for(int i=0; i<n;i++){
-        int minIndex = i;
-        for(int j=i+1; j<n ;j++){
-            if(arr[j] < arr[minIndex]){
-                minIndex = j;
-            }
-        }
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+void mergeSort(int* arr, int s, int e){
+    if(s>=e){
+        return;
     }
+
+    int mid = s + (e-s)/2;
+
+    mergeSort(arr, s, mid);
+    mergeSort(arr, mid+1, e);
+
+    merge(arr, s,e);
+
 }
 
-void insertionSort(int* arr, int n){
-    int key, i, j;
-    for(i=1; i<n; i++){
-        key = arr[i];
-        j = i-1;
-        while(j>=0 && arr[j] > key){
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1] = key;
-    }
-}
-
-
-int main(){
+int main() {
     int n;
     cin>>n;
 
     int arr[n];
-
-    for(int i=0;i<n;i++){
+    for(int i =0;i<n;i++){
         cin>>arr[i];
     }
 
-    selectionSort(arr,n);
-    display(arr, n);
-    // for(int i=0;i<n;i++){
-    //     cout<<arr[i]<<" ";
-    // }
-    // cout<<endl;
+    mergeSort(arr, 0, n-1);
 
-    return 0;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
 }
--- Codes/tempCodeRunnerFile.cpp
+++ Codes/tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
 
-    // cout<<factorial(n)<<endl;
