00099a311c2435f6d94a40f24a36c70da338febc
hardik jain <hardikj2003@gmail.com>
2026-03-09 17:28:40 +0530

    mergeSort revised

--- Codes/revision.cpp
+++ Codes/revision.cpp
@@ -1,48 +1,52 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 void merge(int* arr, int s, int e){
     int mid = s + (e-s)/2;
 
-    int len1 = mid-s+1;
+    int len1 = mid - s +1;
     int len2 = e-mid;
 
     int* first = new int[len1];
     int* second = new int[len2];
 
-    int k = s;
-    for(int i=0; i<len1; i++){
+    int k=s;
+
+    for(int i=0;i<len1; i++){
         first[i] = arr[k++];
     }
-    k=mid+1;
+
+    k = mid+1;
     for(int i=0;i<len2; i++){
         second[i] = arr[k++];
     }
 
-    int index1 =0;
-    int index2=0;
+    int index1 = 0;
+    int index2 = 0;
 
-    k = s;
+    k=s;
 
-    while(index1<len1 && index2<len2){
+    while(index1 < len1 && index2 < len2){
         if(first[index1] < second[index2]){
             arr[k++] = first[index1++];
         }else{
             arr[k++] = second[index2++];
         }
-    }   
-
-    while(index1<len1){
-        arr[k++] = first[index1++];
     }
-    while(index2<len2){
-        arr[k++] = second[index2++];
+
+     while(index1 < len1){
+        arr[k++] = first[index1++]; 
+    }
+
+    while(index2 < len2){
+        arr[k++] = second[index2++]; 
     }
 
     delete []first;
     delete []second;
 
 }
+
 
 void mergeSort(int* arr, int s, int e){
     if(s>=e){
@@ -51,11 +55,10 @@ void mergeSort(int* arr, int s, int e){
 
     int mid = s + (e-s)/2;
 
-    mergeSort(arr, s, mid);
-    mergeSort(arr, mid+1, e);
+    mergeSort(arr,s, mid);
+    mergeSort(arr,mid+1, e);
 
-    merge(arr, s,e);
-
+    merge(arr, s, e);
 }
 
 int main() {
@@ -63,8 +66,8 @@ int main() {
     cin>>n;
 
     int arr[n];
-    for(int i =0;i<n;i++){
-        cin>>arr[i];
+    for(int i=0;i<n;i++){
+        cin>>arr[i]; 
     }
 
     mergeSort(arr, 0, n-1);
@@ -73,4 +76,6 @@ int main() {
         cout<<arr[i]<<" ";
     }
 
+    cout<<endl;
+    return 0;
 }
